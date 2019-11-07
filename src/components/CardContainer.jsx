6be48cba8b76e8b8b3c4dfaae7c4c8a29cee2b44
Christopher Herrington <christopher.herrington@outlook.com>
2019-11-07 09:53:56 -0500

    init

---
+++ src/components/CardContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Card from './Card';
+
+class CardContainer extends React.Component {
+    
+    
+    // render() {
+
+    // }
+}
+
+export default CardContainer;
